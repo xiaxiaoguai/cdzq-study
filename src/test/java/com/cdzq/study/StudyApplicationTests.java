43476dc8676bec1d62650cee8d7eff0178e26eae
52190613@qq.com <52190613@qq.com>
2021-06-18 20:43:10 +0800

    Initial commit

---
+++ src/test/java/com/cdzq/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdzq.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
